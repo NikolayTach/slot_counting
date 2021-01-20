16ac4e9fa355087bf8a065fe0308837215a4fb37
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-01-20 21:58:34 +0000

    Add files via upload

---
+++ slot-counter2333.rs
@@ -0,0 +1,13 @@
+ 
+!#[ Moderation.1, Moderation.2, Moderation.3]
+!#oppected at limda
+!#shown::odd  
+!#array::toe$#binde::due('2333')
+   roor::( bind slot-conter/etc/limda/ )
+ 
+!#dkeenkon nothing 
+
+hog den::find path at 
+#https://github/NikolayTach/slot-counter/path.d <--- makes a refernce link to the person who is looking at it, thank you.#
+
+end
