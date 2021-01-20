2d56162773e15db9c632087edd939ffa6440ad12
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-01-21 01:35:13 +0300

    Update slot-counter2333.rs

--- slot-counter2333.rs
+++ slot-counter2333.rs
@@ -1,10 +1,10 @@
  
-!#[ Moderation.1, Moderation.2, Moderation.3]
+!#[Moderation.1, Moderation.2, Moderation.3]
 !#oppected at limda
 !#shown::odd  
 !#array::toe$#binde::due('2333')
    roor::( bind slot-conter/etc/limda/ )
- 
+ en{}
 !#dkeenkon nothing 
 
 hog den::find path at 
