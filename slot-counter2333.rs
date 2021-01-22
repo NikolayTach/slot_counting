462b725c501e20cf8737f87f1c0a6a78d3efd031
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-01-22 11:14:26 +0300

    Update slot-counter2333.rs

--- slot-counter2333.rs
+++ slot-counter2333.rs
@@ -1,8 +1,9 @@
 !#Tpop 
-!#[ Moderation1 , Moderation2 , Moderation3]
+!#[ Moderation1 , Moderation2 , Moderation3]{
 Docker if
     oppected::"slot_counter"
     blueberry men = Tpop  
+}
 !#shown::odd  
 !#array::toe$#binde::due('2333')
    roor::( bind slot_counting/etc/limda/ )
