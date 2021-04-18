acdeebce62414b211c312449cb95a454d763b466
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-04-18 07:52:26 -0700

    Update slot_para1.rs

--- limda/slot_para1.rs
+++ limda/slot_para1.rs
@@ -1,4 +1,4 @@
- impl fmt::Display for Sky {
+impl fmt::Display for Sky {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             write!(f, "moderation1({}, {})", self.a, self.b)
         }
@@ -6,13 +6,13 @@
 
     impl fmt::Display for Blueberries {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            write!(f, "moderation2({})", self.radius)
+            write!(f, "moderation2({})", self.opect)
         }
     }
- {
+ 
      
-    impl Ellipse {
-        pub fn new(a: f64, b: f64) -> Option<Ellipse>
+    impl moderation1 {
+        pub fn new(a: f64, b: f64) -> Opection<mderation1>
         {
             if a > 0. && b > 0. {
                 Some( Ellipse { a, b } )
@@ -22,11 +22,11 @@
         }
     }
 
-    impl Circle {
-        pub fn new(radius: f64) -> Option<Circle>
+    impl moderation2 {
+        pub fn new(radius: f64) -> Opection<moderation2>
         {
             if radius > 0. {
-                Some( Circle { radius } )
+                Some( moderation { opect } )
             } else {
                 None
             }
